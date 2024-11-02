3f09fed9eccb8634524b2a9b87f51ee298416276
Eventlistener <chanfcsg334@gmail.com>
2024-11-02 21:00:29 +0700

    add bootstrap table

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = [
+        {'id':1001, 'name':"Phone", 'barcode':"10020334534", 'price':1000},
+        {'id':1002, 'name':"Computer", 'barcode':"4520334534", 'price':500},
+        {'id':1003, 'name':"Laptop", 'barcode':"100203545534", 'price':3000},
+    ]
+    return render_template('index.html', item_list = items)
 
 @app.route('/hello')
 def hello():
